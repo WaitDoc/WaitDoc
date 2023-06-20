b0b5635fece2549b411431fb057c0ab01c06a400
Y39 <rla4878@gmail.com>
2023-06-20 16:36:47 +0900

    refactor: Util에서 build 패턴을 이용한 url 생성

--- src/main/java/com/team13/WaitDoc/Category/controller/CategoryController.java
+++ src/main/java/com/team13/WaitDoc/Category/controller/CategoryController.java
@@ -1,21 +1,14 @@
 package com.team13.WaitDoc.Category.controller;
 
 import com.team13.WaitDoc.base.config.AppConfig;
+import com.team13.WaitDoc.base.util.Ut;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -28,16 +21,16 @@ public class CategoryController {
     @GetMapping("/all")
     @ResponseBody
     public String showHospital() throws IOException, InterruptedException {
-        String serviceKey = AppConfig.getServiceKey();
-        StringBuilder urlBuilder = new StringBuilder(AppConfig.getApiUrl()); //URL
 
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8")
-                        + "=" + URLEncoder.encode(serviceKey,"UTF-8")); //Service Key
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8")
-                        + "=" + URLEncoder.encode(String.valueOf(AppConfig.getRows()), "UTF-8")); //한 페이지 결과 수
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8")
-                        + "=" + URLEncoder.encode("1", "UTF-8")); //페이지 번호
-        URL url = new URL(urlBuilder.toString());
+        String u = Ut.ApiUrl.builder()
+                .page(1)
+                .rows(10)
+                .region("서울특별시")
+                .addr("서대문구")
+                .name("삼성")
+                .build();
+        System.out.println(u);
+        URL url = new URL(u);
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
--- src/main/java/com/team13/WaitDoc/base/util/Ut.java
+++ src/main/java/com/team13/WaitDoc/base/util/Ut.java
@@ -1,5 +1,57 @@
 package com.team13.WaitDoc.base.util;
 
+import com.team13.WaitDoc.base.config.AppConfig;
+import lombok.AllArgsConstructor;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class Ut {
+    @AllArgsConstructor
+    public static class  ApiUrl {
+        StringBuilder ub;
+
+        public static ApiUrl builder() throws UnsupportedEncodingException{
+            String serviceKey = AppConfig.getServiceKey();
+
+            StringBuilder sb = new StringBuilder(AppConfig.getApiUrl()).append("?" + URLEncoder.encode("ServiceKey","UTF-8")
+                    + "=" + URLEncoder.encode(serviceKey,"UTF-8")); //Service Key
+
+            return new ApiUrl(sb);
+        }
+
+        public ApiUrl page(int page)throws UnsupportedEncodingException{
+            ub.append("&" + URLEncoder.encode("pageNo","UTF-8")
+                    + "=" + URLEncoder.encode(String.valueOf(page), "UTF-8")); //페이지 번호
+            return this;
+        }
+
+        public ApiUrl rows(int rows)throws UnsupportedEncodingException{
+            ub.append("&" + URLEncoder.encode("numOfRows","UTF-8")
+                    + "=" + URLEncoder.encode(String.valueOf(rows), "UTF-8")); //페이지 번호
+            return this;
+        }
+
+        public ApiUrl region(String region) throws UnsupportedEncodingException{
+            ub.append("&" + URLEncoder.encode("Q0","UTF-8")
+                    + "=" + URLEncoder.encode(region, "UTF-8"));
+            return this;
+        }
+        public ApiUrl addr(String addr) throws UnsupportedEncodingException{
+            ub.append("&" + URLEncoder.encode("Q1","UTF-8")
+                    + "=" + URLEncoder.encode(addr, "UTF-8"));
+            return this;
+        }
+        public ApiUrl name(String name) throws UnsupportedEncodingException{
+            ub.append("&" + URLEncoder.encode("QN","UTF-8")
+                    + "=" + URLEncoder.encode(name, "UTF-8"));
+            return this;
+        }
+
+        public String build(){
+            return this.ub.toString();
+        }
+    }
+
 
 }
