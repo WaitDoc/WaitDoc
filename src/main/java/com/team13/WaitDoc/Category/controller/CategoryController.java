52642ffd74e8e43c1e8e7f4d6a0763cbb96dd46d
Y39 <rla4878@gmail.com>
2023-06-20 15:06:13 +0900

    Refactor: AppConfig 도입으로 변수값 관리

--- src/main/java/com/team13/WaitDoc/Category/controller/CategoryController.java
+++ src/main/java/com/team13/WaitDoc/Category/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.team13.WaitDoc.Category.controller;
 
+import com.team13.WaitDoc.base.config.AppConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -27,16 +28,17 @@ public class CategoryController {
     @GetMapping("/all")
     @ResponseBody
     public String showHospital() throws IOException, InterruptedException {
-        String serviceKey = "KCbIRVK3eNx25olr0o1K/FlFOwMZasPPJqbuHvXG7ylFSvedOw0XmLHJAFpNsn4yoqN/XDOBbe6/2FyF8iCQ+Q==";
-        StringBuilder urlBuilder = new StringBuilder("https://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncFullDown"); /*URL*/
-        // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
+        String serviceKey = AppConfig.getServiceKey();
+        StringBuilder urlBuilder = new StringBuilder(AppConfig.getApiUrl()); //URL
+
         urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8")
-                        + "=" + URLEncoder.encode(serviceKey,"UTF-8")); /*Service Key*/
+                        + "=" + URLEncoder.encode(serviceKey,"UTF-8")); //Service Key
         urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8")
-                        + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+                        + "=" + URLEncoder.encode(String.valueOf(AppConfig.getRows()), "UTF-8")); //한 페이지 결과 수
         urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8")
-                        + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+                        + "=" + URLEncoder.encode("1", "UTF-8")); //페이지 번호
         URL url = new URL(urlBuilder.toString());
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url.toString()))
@@ -48,27 +50,6 @@ public class CategoryController {
         int responseCode = response.statusCode();
         System.out.println(response.body());
         return response.body();
-
-        //        URL url = new URL(urlBuilder.toString());
-//        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//        conn.setRequestMethod("GET");
-//        conn.setRequestProperty("Content-type", "application/json");
-//        System.out.println("Response code: " + conn.getResponseCode());
-//        BufferedReader rd;
-//        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-//            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//        } else {
-//            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-//        }
-//        StringBuilder sb = new StringBuilder();
-//        String line;
-//        while ((line = rd.readLine()) != null) {
-//            sb.append(line);
-//        }
-//        rd.close();
-//        conn.disconnect();
-//        return sb.toString();
-//        return null;
     }
 
 }
