a278f8619f4a5dc33c39242817c59a4f42d57a71
HamSeungChan <head0618@naver.com>
2023-06-21 15:22:35 +0900

    feat: Member Entity 생성

---
+++ src/main/java/com/team13/WaitDoc/member/entity/Member.java
@@ -0,0 +1,35 @@
+package com.team13.WaitDoc.member.entity;
+
+import com.team13.WaitDoc.base.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Column(nullable = false)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private MemberRole memberRole;
+
+}
