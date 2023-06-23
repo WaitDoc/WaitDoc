c153114cacaf615322dff9694e86bb715de8905a
HamSeungChan <head0618@naver.com>
2023-06-23 16:43:22 +0900

    feat: 네이버 로그인 구현

--- src/main/java/com/team13/WaitDoc/base/config/auth/OAuthAttributes.java
+++ src/main/java/com/team13/WaitDoc/base/config/auth/OAuthAttributes.java
@@ -32,8 +32,25 @@ public class OAuthAttributes {
         if ("kakao".equals(socialName)) {
             return ofKakao(userNameAttributeName, attributes);
         }
+        //네이버
+        if("naver".equals(socialName)){
+            return ofNaver(userNameAttributeName,attributes);
+        }
 
         return null;
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .nickname((String) response.get("name"))
+                .email((String) response.get("email"))
+                .gender((String)response.get("gender"))
+                .birthday((String)response.get("birthday"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
