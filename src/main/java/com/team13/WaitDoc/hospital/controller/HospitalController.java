97756377db505c18c495259645e05bd0cd5629e9
Cualone <june568@naver.com>
2023-06-28 05:42:52 +0900

    Fix : Use KakaoMap Search for Map instead data.go.kr
    공공데이터 대신 카카오맵 카테고리 검색 활용하여 지도 표시

--- src/main/java/com/team13/WaitDoc/hospital/controller/HospitalController.java
+++ src/main/java/com/team13/WaitDoc/hospital/controller/HospitalController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.io.IOException;
 import java.util.List;
 
@@ -17,23 +16,10 @@ public class HospitalController {
 
     @GetMapping("/map")
     public String mapHospital(Model model) throws IOException, InterruptedException {
-        String url = ApiUt.ApiUrl.builder()
-                .page(1)
-                .rows(2)
-                .region("서울특별시")
-                .addr("서대문구")
-                .name("삼성")
-                .build();
 
         model.addAttribute("appKey", AppConfig.getMapKey());
-
-        List<HospitalXml.Item> items = ApiUt.ApiXml.getItems(ApiUt.ApiResponse.getResult(url));
-
-        System.out.println("Generated URL: " + ApiUt.ApiResponse.getResult(url));
-
-
-        model.addAttribute("items", items);
-
         return "hospital/map";
     }
 }
+
+
