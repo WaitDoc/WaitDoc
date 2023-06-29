4a36bc16428e027bb1ecc38d560492bb4f5c67d7
Cualone <125931703+Cualone@users.noreply.github.com>
2023-06-29 15:52:42 +0900

    주변 병원/약국 탐색 (#43)
    * Feat : Map Init
    
    카카오맵, 공공 데이터 연결 (현재 둘 이상의 데이터를 불러오는 데 오류가
    발생함)
    
    * Fix : set Center Coordinates to My Location
    
    중심 좌표 현재 내 위치로
    
    * Feat : Change Center Coordinates by Receiving an Address
    
    주소를 입력 받아 중심 좌표를 변경
    
    * Fix : Use KakaoMap Search for Map instead data.go.kr
    
    공공데이터 대신 카카오맵 카테고리 검색 활용하여 지도 표시
    
    * Fix : Fix Map Size
    
    지도 비율 수정
    
    * Delete gitmessage.txt
    
    * Fix : Remove Garbage

--- src/main/java/com/team13/WaitDoc/hospital/controller/HospitalController.java
+++ src/main/java/com/team13/WaitDoc/hospital/controller/HospitalController.java
@@ -1,9 +1,22 @@
 package com.team13.WaitDoc.hospital.controller;
 
+
+import com.team13.WaitDoc.base.config.AppConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.io.IOException;
+
 
 @Controller
 @RequiredArgsConstructor
 public class HospitalController {
+
+    @GetMapping("/map")
+    public String mapHospital(Model model) throws IOException, InterruptedException {
+
+        model.addAttribute("appKey", AppConfig.getMapKey());
+        return "hospital/map";
+    }
 }
