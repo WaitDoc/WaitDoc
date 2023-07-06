de18a7be3e632da0ab251ac3313726132eaec104
Heeesuu <gmltn0508@icloud.com>
2023-07-06 10:17:34 +0900

    fix: member 데이터가 초기화되지 않는 문제해결

---
+++ src/main/generated/com/team13/WaitDoc/hospital/entity/QDepartment.java
@@ -0,0 +1,62 @@
+package com.team13.WaitDoc.hospital.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QDepartment is a Querydsl query type for Department
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QDepartment extends EntityPathBase<Department> {
+
+    private static final long serialVersionUID = 1545627638L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QDepartment department = new QDepartment("department");
+
+    public final com.team13.WaitDoc.base.entity.QBaseEntity _super = new com.team13.WaitDoc.base.entity.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createDate = _super.createDate;
+
+    public final QHospital hospital;
+
+    //inherited
+    public final NumberPath<Long> id = _super.id;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifyDate = _super.modifyDate;
+
+    public final StringPath name = createString("name");
+
+    public QDepartment(String variable) {
+        this(Department.class, forVariable(variable), INITS);
+    }
+
+    public QDepartment(Path<? extends Department> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QDepartment(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QDepartment(PathMetadata metadata, PathInits inits) {
+        this(Department.class, metadata, inits);
+    }
+
+    public QDepartment(Class<? extends Department> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.hospital = inits.isInitialized("hospital") ? new QHospital(forProperty("hospital")) : null;
+    }
+
+}
+
--- src/main/generated/com/team13/WaitDoc/member/entity/QMember.java
+++ src/main/generated/com/team13/WaitDoc/member/entity/QMember.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -22,6 +23,8 @@ public class QMember extends EntityPathBase<Member> {
     public final com.team13.WaitDoc.base.entity.QBaseEntity _super = new com.team13.WaitDoc.base.entity.QBaseEntity(this);
 
     public final StringPath birthday = createString("birthday");
+
+    public final ListPath<com.team13.WaitDoc.blacklist.entity.Blacklist, com.team13.WaitDoc.blacklist.entity.QBlacklist> blacklists = this.<com.team13.WaitDoc.blacklist.entity.Blacklist, com.team13.WaitDoc.blacklist.entity.QBlacklist>createList("blacklists", com.team13.WaitDoc.blacklist.entity.Blacklist.class, com.team13.WaitDoc.blacklist.entity.QBlacklist.class, PathInits.DIRECT2);
 
     //inherited
     public final DateTimePath<java.time.LocalDateTime> createDate = _super.createDate;
@@ -39,6 +42,8 @@ public class QMember extends EntityPathBase<Member> {
     public final DateTimePath<java.time.LocalDateTime> modifyDate = _super.modifyDate;
 
     public final StringPath name = createString("name");
+
+    public final ListPath<com.team13.WaitDoc.waiting.entity.Waiting, com.team13.WaitDoc.waiting.entity.QWaiting> waitings = this.<com.team13.WaitDoc.waiting.entity.Waiting, com.team13.WaitDoc.waiting.entity.QWaiting>createList("waitings", com.team13.WaitDoc.waiting.entity.Waiting.class, com.team13.WaitDoc.waiting.entity.QWaiting.class, PathInits.DIRECT2);
 
     public QMember(String variable) {
         super(Member.class, forVariable(variable));
