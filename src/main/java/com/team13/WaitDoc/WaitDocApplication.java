5ec698aefd7039704690e659dc1a66a599e88b24
jojeongbin <cjb1009@naver.com>
2023-07-07 21:38:21 +0900

    style : 코드 복구

--- src/main/java/com/team13/WaitDoc/WaitDocApplication.java
+++ src/main/java/com/team13/WaitDoc/WaitDocApplication.java
@@ -2,12 +2,15 @@ package com.team13.WaitDoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WaitDocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaitDocApplication.class, args);
 	}
+
 
 }
