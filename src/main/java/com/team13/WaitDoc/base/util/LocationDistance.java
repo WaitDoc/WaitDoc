e9d74d790ec24941804be264eae48078830e3ea1
Y39 <rla4878@gmail.com>
2023-07-11 20:35:00 +0900

    Feat: 현재 위치에 따른 거리에 따라서 정렬

--- src/main/java/com/team13/WaitDoc/base/util/LocationDistance.java
+++ src/main/java/com/team13/WaitDoc/base/util/LocationDistance.java
@@ -1,7 +1,7 @@
 package com.team13.WaitDoc.base.util;
 
 public class LocationDistance {
-    private static double calc(double lat1, double lon1, double lat2, double lon2) {
+    public static Long calc(double lat1, double lon1, double lat2, double lon2) {
 
         double theta = lon1 - lon2;
         double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
@@ -11,7 +11,7 @@ public class LocationDistance {
         dist = dist * 60 * 1.1515;
         dist = dist * 1609.344;
 
-        return (dist);
+        return (long) dist;
     }
 
 
