d9cb1d40ce00425700a740f750ef442f4f3398a3
Heeesuu <gmltn0508@icloud.com>
2023-07-11 09:48:46 +0900

    feat: 마이페이지 중복로그인시 안들어가지던 오류 수정

--- src/main/java/com/team13/WaitDoc/member/controller/MemberController.java
+++ src/main/java/com/team13/WaitDoc/member/controller/MemberController.java
@@ -1,9 +1,12 @@
 package com.team13.WaitDoc.member.controller;
 
 
+import com.team13.WaitDoc.base.config.auth.SessionMember;
 import com.team13.WaitDoc.member.entity.Member;
 import com.team13.WaitDoc.member.service.MemberService;
 import com.team13.WaitDoc.security.SecurityUser;
+
+import jakarta.servlet.http.HttpSession;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +30,16 @@ public class MemberController {
     }
 
     @GetMapping("/mypage")
-    public String showMyPage(Model model, @AuthenticationPrincipal SecurityUser securityUser) {
-        Member member = memberService.findByName(securityUser.getName());
-        model.addAttribute("member", member);
+    public String showMyPage(Model model, HttpSession session) {
+        Object memberObj = session.getAttribute("member");
+        if (memberObj instanceof SessionMember) {
+            SessionMember sessionMember = (SessionMember) memberObj;
+            String memberName = sessionMember.getName();
+            model.addAttribute("member", memberName);
+        } else {
+            // handle error or redirect to login page
+        }
+
         return "member/mypage";
     }
 
